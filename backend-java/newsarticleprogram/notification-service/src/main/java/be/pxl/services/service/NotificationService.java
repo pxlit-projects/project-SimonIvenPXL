f2bf593c3fc21b0b8facda5e28010b1277f2852f
SimonIvenPXL <simon.iven@student.pxl.be>
2025-01-12 19:30:57 +0100

    Added RabbitMQ

--- backend-java/newsarticleprogram/notification-service/src/main/java/be/pxl/services/service/NotificationService.java
+++ backend-java/newsarticleprogram/notification-service/src/main/java/be/pxl/services/service/NotificationService.java
@@ -1,11 +1,13 @@
 package be.pxl.services.service;
 
 import be.pxl.services.domain.Notification;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class NotificationService {
     private static final Logger log = LoggerFactory.getLogger(NotificationService.class);
 
